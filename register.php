858cd3d2f6b7219f84caaa8847f02bc2ea320bce
Jose Munoz <jommunoz90@gmail.com>
2023-06-26 10:15:46 +0100

    updates

--- register.php
+++ register.php
@@ -1,43 +1,5 @@
 <?php
 require_once('config.php');
-//require_once "config.php";
-//require_once "session.php";
-
-//if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
-
-  //  $fullname = trim($_POST['name']);
-   // $email = trim($_POST['email']);
-
-    //if($query = $db->prepare("SELECT * FROM users WHERE email = ?")){
-      //  $error = '';
-
-        // Bind parameters (s = string, i = int, b = blob, etc.). In this case, the username is a
-        // string, so we use 's'.
-        //$query->bind_param('s', $email);
-        //$query->execute();
-
-        // Store the result sso that we can check if the account already exists in the database.
-        //$query->store_result();
-        //if($query->num_rows > 0){
-          //  $error .= '<p class="error"> The email address is already registered!</p>';
-        //} else {
-          //  if(empty($error)){
-            //    $insertQuery = $db->prepare("INSERT INTO users (name, email) VALUES (?, ?, ?);");
-              //  $insertQuery->bind_param("sss", $fullname, $email);
-                //$result = $insertQuery->execute();
-                //if($result){
-                  //  $error .= '<p class ="success"> Your registration was successful!</p>';
-                //} else {
-                  //  $error .= '<p class = "error"> Something went wrong!</p>';
-                //}
-            //}
-        //}
-    //}
-    //$query->close();
-    //$insertQuery->close();
-    // Close DB connection
-    //mysqli_close($db);
-//}
 ?>
 <!DOCTYPE html>
 <html>
