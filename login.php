b0bfdf9785c5d60ae2ac0cfdb1e1e046f090ac0a
Jose Munoz <jommunoz90@gmail.com>
2023-06-26 09:40:00 +0100

    updates

--- login.php
+++ login.php
@@ -1,43 +1,5 @@
 <?php
 require_once('config.php');
-//require_once "config.php";
-//require_once "session.php";
-
-//if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])){
-
-  //  $fullname = trim($_POST['name']);
-   // $email = trim($_POST['email']);
-
-    //if($query = $db->prepare("SELECT * FROM users WHERE email = ?")){
-      //  $error = '';
-
-        // Bind parameters (s = string, i = int, b = blob, etc.). In this case, the username is a
-        // string, so we use 's'.
-        //$query->bind_param('s', $email);
-        //$query->execute();
-
-        // Store the result sso that we can check if the account already exists in the database.
-        //$query->store_result();
-        //if($query->num_rows > 0){
-          //  $error .= '<p class="error"> The email address is already registered!</p>';
-        //} else {
-          //  if(empty($error)){
-            //    $insertQuery = $db->prepare("INSERT INTO users (name, email) VALUES (?, ?, ?);");
-              //  $insertQuery->bind_param("sss", $fullname, $email);
-                //$result = $insertQuery->execute();
-                //if($result){
-                  //  $error .= '<p class ="success"> Your registration was successful!</p>';
-                //} else {
-                  //  $error .= '<p class = "error"> Something went wrong!</p>';
-                //}
-            //}
-        //}
-    //}
-    //$query->close();
-    //$insertQuery->close();
-    // Close DB connection
-    //mysqli_close($db);
-//}
 ?>
 <!DOCTYPE html>
 <html>
@@ -71,28 +33,17 @@ require_once('config.php');
     <div class="container">
         <div class="form-box">
             <img src="img/bigcommerce.png" height="20%" width="40%">
-            <h1 id="title">Sign Up</h1>
+            <h1 id="title">Sign In</h1>
             <form action="" method="post">
                 <div class="input-group">
-                    <div class="input-field" id="nameField">
-                        <i class="fa-solid fa-user-tie"></i>
-                        <input type="text" placeholder="First Name" name="firstname" required>
-                    </div>
-
-                    <div class="input-field" id="nameField">
-                        <i class="fa-solid fa-user-tie"></i>
-                        <input type="text" placeholder="Last Name" name="lastname" required>
-                    </div>
-
                     <div class="input-field">
                         <i class="fa-solid fa-at"></i>
                         <input type="email" placeholder="Email" name="email" required>
                     </div>
-
                 </div>
                 <div class="btn-field">
                     <button type="submit" name="create" value="Sign Up" id="signupBtn" class="disable">Sign UP</button>
-                    <button type="submit" id="signinBtn" class="disable"><a href="login.php">Sign IN</a></button>
+                    <button type="submit" id="signinBtn" class="disable">Sign In</button>
                 </div>
             </form>
         </div>
---
+++ welcome.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="with=device-width, initial-scale=1.0">
+    <title>WELCOME</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="https://kit.fontawesome.com/bad3a99581.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <h1>Hello, shopping lovers.</h1>
+    <h2>Welcome to Bigcommerce</h2>
+    <a href="logout.php">Log Out</a>
+</body>
+</html>
