b9669170aa6f6689f84d05306926c0e7acb91bc6
Jose Munoz <jommunoz90@gmail.com>
2023-06-29 10:17:21 +0100

    updates

--- welcome.php
+++ welcome.php
@@ -21,8 +21,30 @@ session_start();
     </head>
 
     <body>
-        <li><a href="logout.php">Sign Out</a></li>
-        <h1>Hello, <strong><?php echo $_SESSION["firstname"]; ?></strong>! Welcome to BigCommerce.</h1>
-        <h2>Who We Are & What We Do</h2>
+
+    <ul class="navbar">
+    <li><a href="#about">About Us</a></li>
+    <li><a href="#values">Company Values</a></li>
+    <li><a href="#others">Other</a></li>
+    <li><a href="logout.php">Sign Out</a></li>
+    </ul>
+
+    <h1>Hello, <strong><?php echo $_SESSION["firstname"]; ?></strong>! Welcome to BigCommerce.</h1>
+    
+    <!--About Us section-->
+    <section class="about" id="about">
+        <div class="about-text">
+            <h2>Who We Are & What We Do</h2>
+            <p>BigCommerce's mission is to help merchants sell more at every stage of growth, 
+            from small startups, to mid-market businesses, 
+            to large enterprises. As a leading Open SaaS solution, 
+            BigCommerce empowers merchants to build, innovate and grow their businesses online. 
+            Simply put, we focus on being the best commerce platform so our customers can focus 
+            on what matters most: growing their businesses.</p>
+            <p>Learn more about our growing </p><a href="https://www.bigcommerce.com/company/awards/">industry-wide recognition.</a>
+        </div>
+    </section>
+
+    <!--Other section-->
     </body>
 </html>
